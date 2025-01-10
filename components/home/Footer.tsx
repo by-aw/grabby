0754eb6b542e2ceefa63fae285a20ae57eb0fa84
allwhite <117570049+by-aw@users.noreply.github.com>
2025-01-09 20:47:38 -0500

    Fix footer layout

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -1,10 +1,18 @@
 "use client";
 
 import useLocalStorage from "@/hooks/useLocalStorage";
+import { useEffect, useState } from "react";
 import Features from "./Features";
 import UserLinks from "./UserLinks";
 
 export default function Footer() {
-  const savedLinks = useLocalStorage<string[]>("links", []);
+  const [savedLinks] = useLocalStorage<string[]>("links", ["loading"]);
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <></>;
+  }
   return <>{savedLinks.length > 0 ? <UserLinks /> : <Features />}</>;
 }
